a8174bb3aa93e5121f1bfbc504c74bd9d103f387
ramethaa <jey_rame@yahoo.com>
2015-06-26 10:45:24 +1000

    Modified

--- trialRforgithub.R
+++ trialRforgithub.R
@@ -1,6 +1,14 @@
+#trial R code for Git hub version control
+
 data<-read.csv(file.choose())
 names(data)
 dim(data)
 head(data)
 summary(data)
 
+attach(data)
+
+#tell R that output is a categorical variable rather than a number
+
+data$cat <- factor(data$species, level=c(0,1), label=c("absent","present"))
+data$calcretene <- factor(data$calcretene, level=c(0,1), label=c("absent","present"))
